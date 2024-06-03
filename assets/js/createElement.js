7ceb92907f8b01b42544114dc6c8f599f5ad0c53
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-03 12:16:14 +0300

    Add fetch

--- assets/js/createElement.js
+++ assets/js/createElement.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
-options={
+const options={
   classNames : ['actors', 'active'],
   attributes : {'title': 'actors', 'key':'value'},
   styles : {'color':'red'},
@@ -10,9 +10,19 @@ options={
 */
 
 function createElement(tag, options, ...children) {
-  const elem = document.createElement(tag);
-  if(options['classNames']){
-    elem.classList.add(...options['classNames']);
+  const elem = document.createElement(tag); 
+  if(options.classNames){
+    elem.classList.add(...options.classNames);
+  }
+  if(options.attributes){
+    for (const titleAttr in options.attributes) {  //titleAttr = 'title', titleAttr='key'
+      elem.setAttribute(titleAttr, options.attributes[titleAttr])
+    }
+  }
+  if(options.events){
+    for (const titleEvent in options.events) { 
+      elem.addEventListener(titleEvent, options.events[titleEvent])
+    }
   }
   elem.append(...children);
   return elem;
--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 'use strict';
-
 const root = document.getElementById('root');
 
-// const section = document.createElement('section');
-// section.classList.add('actors', 'active');
-
-// const h1 = document.createElement('h1');
-// h1.classList.add('actors-heading');
-// h1.append('Actors');
-
-// section.append(h1);
-
-const h1 = createElement(
-  'h1',
-  { classNames: ['actors-heading', 'upper-case'] },
-  'Actors'
-);
-const section = createElement('section', { classNames: ['actors'] }, h1);
-
-root.append(section);
+fetch('/assets/js/data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const h1 = createElement(
+      'h1',
+      {
+        classNames: ['actors-heading', 'upper-case'],
+        attributes: { title: 'qwerty', 'data-key': 22 },
+      },
+      'Actors'
+    );
+    const articles = data.map((user) => {
+      const img = createElement('img', {
+        attributes: { src: user.profilePicture },
+        events: {'error': ({target})=>{target.remove()}}
+      });
+      return createElement(
+        'article',
+        { classNames: ['actor'] },
+        user.firstName, img
+      );
+    });
+    const section = createElement(
+      'section',
+      { classNames: ['actors'] },
+      h1,
+      ...articles
+    );
+    root.append(section);
+  })
+  .catch((error) => {
+    root.append('try again'); //404
+  });
